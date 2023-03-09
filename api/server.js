009b67e0c6862eb63d592b15eeeeaf45fecf329d
Robert Gripshover <robertgrips@gmail.com>
2023-03-09 11:06:30 -0500

    made changes to the server file

--- api/server.js
+++ api/server.js
@@ -1,5 +1,23 @@
 const express = require('express');
+
+const projectsRouter = require('./projects/projects-router')
+
+const actionsRouter = require('./actions/actions-router')
+
 const server = express();
+
+server.use(express.json())
+
+server.use('/api/projects', projectsRouter)
+
+server.use('/api/actions', actionsRouter)
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
