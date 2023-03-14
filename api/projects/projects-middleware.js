b4cd5a2e48adb21138f268dae419953ddf6b1d9f
Robert Gripshover <robertgrips@gmail.com>
2023-03-14 16:55:02 -0400

    I added a simple logger into the project-middleware.js file to text out my server

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,17 @@
 // add middlewares here related to projects
+
+const Projects = require('./projects-model')
+
+function logger(req, res, next) {
+    console.log('logger middleware')
+    const timeStamp = new Date().toLocaleString()
+    const method = req.method
+    const url = req.originalUrl
+    console.log(`[${timeStamp}] ${method} to ${url}`)
+    next();
+}
+
+
+module.exports = {
+    logger,
+}
