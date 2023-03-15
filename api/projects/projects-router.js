cad9920b6a7fa29790e98df1f2cf3ab7bb669964
Robert Gripshover <robertgrips@gmail.com>
2023-03-15 10:57:37 -0400

    wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -21,6 +21,14 @@ router.get('/', (req, res, next) => {
 
 router.get('/:id', validateProjectId, (req, res) => {
     res.json(req.project)
-  });
+});
+
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        customMessage: 'something tragic happened inside the projects router',
+        message: err.message,
+        stack: err.stack,
+    })
+})
 
 module.exports = router
